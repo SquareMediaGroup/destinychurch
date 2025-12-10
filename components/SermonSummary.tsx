7e60c116adf82daf66d28e30a6c7c6eb1f96ab7d
Square Media Group <malachi@squaremediagroup.org>
2025-12-10 17:42:54 +0000

    Wrap markdown summary in styled container to satisfy types

--- components/SermonSummary.tsx
+++ components/SermonSummary.tsx
@@ -38,25 +38,26 @@ export default function SermonSummary({ summary }: SermonSummaryProps) {
   return (
     <div className="rounded-xl border border-black/5 bg-white px-4 py-4 shadow-sm sm:px-5">
       <p className="subheading text-sm text-destiny-orange">Summary</p>
-      <ReactMarkdown
-        className="mt-2 space-y-3 text-sm leading-relaxed text-destiny-grey"
-        components={{
-          p: (props) => <p className="leading-relaxed text-destiny-grey" {...props} />,
-          strong: (props) => <strong className="font-semibold text-destiny-black" {...props} />,
-          em: (props) => <strong className="font-semibold text-destiny-black" {...props} />,
-          code: (props) => (
-            <code className="rounded bg-destiny-grey/10 px-1 py-0.5 text-[13px] text-destiny-black" {...props} />
-          ),
-          a: (props) => (
-            <a className="font-semibold text-destiny-orange underline" target="_blank" rel="noreferrer" {...props} />
-          ),
-          ul: (props) => <ul className="ml-4 list-disc space-y-2 text-destiny-grey" {...props} />,
-          ol: (props) => <ol className="ml-4 list-decimal space-y-2 text-destiny-grey" {...props} />,
-          li: (props) => <li className="leading-relaxed" {...props} />,
-        }}
-      >
-        {formatted}
-      </ReactMarkdown>
+      <div className="mt-2 space-y-3 text-sm leading-relaxed text-destiny-grey">
+        <ReactMarkdown
+          components={{
+            p: (props) => <p className="leading-relaxed text-destiny-grey" {...props} />,
+            strong: (props) => <strong className="font-semibold text-destiny-black" {...props} />,
+            em: (props) => <strong className="font-semibold text-destiny-black" {...props} />,
+            code: (props) => (
+              <code className="rounded bg-destiny-grey/10 px-1 py-0.5 text-[13px] text-destiny-black" {...props} />
+            ),
+            a: (props) => (
+              <a className="font-semibold text-destiny-orange underline" target="_blank" rel="noreferrer" {...props} />
+            ),
+            ul: (props) => <ul className="ml-4 list-disc space-y-2 text-destiny-grey" {...props} />,
+            ol: (props) => <ol className="ml-4 list-decimal space-y-2 text-destiny-grey" {...props} />,
+            li: (props) => <li className="leading-relaxed" {...props} />,
+          }}
+        >
+          {formatted}
+        </ReactMarkdown>
+      </div>
     </div>
   );
 }

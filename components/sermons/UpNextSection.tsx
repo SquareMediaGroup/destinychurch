442e1a59628c760efc4c27249a3caec9e3d777a3
Square Media Group <malachi@squaremediagroup.org>
2026-03-24 19:03:18 +0000

    Replace material icon with inline SVG for search icon

--- components/sermons/UpNextSection.tsx
+++ components/sermons/UpNextSection.tsx
@@ -85,9 +85,9 @@ export default function UpNextSection({ videos }: UpNextSectionProps) {
         <div className="flex items-center gap-3">
           {/* Filter input */}
           <div className="relative">
-            <span className="material-symbols-rounded pointer-events-none absolute left-3 top-1/2 -translate-y-1/2 text-base text-white/40">
-              search
-            </span>
+            <svg className="pointer-events-none absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-white/40" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+              <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+            </svg>
             <input
               type="text"
               value={filter}
